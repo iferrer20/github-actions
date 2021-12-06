8871d6ec0e94c3cfd73172f8ae2ee339dc656381
iferrer20 <iferreriestacio@gmail.com>
2021-12-06 23:20:58 +0100

    testing

--- .github/actions/update-readme/dist/index.js
+++ .github/actions/update-readme/dist/index.js
@@ -7274,20 +7274,20 @@ const memeMaker = __nccwpck_require__(5594);
 const test_results = true;
 //const test_results = core.getInput('resultado_tests');
 const output_name = (Math.random() + 1).toString(36).substring(7) + '.jpg';
-const meme_image = (test_results ? '../img/good/' : '../img/bad/') + (Math.floor(Math.random() * 3) + 1) + '.jpg';
+const meme_image = (test_results ? '.github/actions/update-readme/img/good/' : '.github/actions/update-readme/img/bad/') + (Math.floor(Math.random() * 3) + 1) + '.jpg';
 
 let options = {
   image: meme_image,
-  outfile: '../output/' + output_name, 
+  outfile: '.github/actions/update-readme/output/' + output_name, 
   topText: core.getInput('frase_positiva'), 
   bottomText: 'Y LO SABES',
-  fontSize: 30
+  fontSize: 20
 }
 
 memeMaker(options, function(err) {
   if (err) throw err;
   console.log('Image saved: ' + options.outfile)
-  fs.appendFileSync('../../../../README.md', `![alt text](https://github.com/iferrer20/github-actions/blob/github_action_readme/.github/actions/update-readme/output/${output_name}?raw=true)\n`);
+  fs.appendFileSync('README.md', `![alt text](https://github.com/iferrer20/github-actions/blob/github_action_readme/.github/actions/update-readme/output/${output_name}?raw=true)\n`);
   core.setOutput('exit-status', 'Meme añadido al readme');
 });
 
